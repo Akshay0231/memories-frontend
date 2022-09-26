fdbf7aef0407f101859145c01569ced7d8f911fb
Akshay0231 <akshaay2301@gmail.com>
2022-09-26 22:10:05 +0530

    App | Material UI Styles package added | makeStyle added & integrated

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import { Container, AppBar, Typography, Grow, Grid } from '@mui/material'
 import Form from './components/Form/Form';
 import Posts from './components/Posts/Posts';
 import memories from './images/memories.svg'
+import useStyles from './styles'
 
 function App() {
+  const classes = useStyles()
+
   return (
     <Container maxWidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant='h2' align='center'>Memories</Typography>
-        <img src={memories} alt="memories" height="60" />
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant='h2' align='center'>Memories</Typography>
+        <img className={classes.image} src={memories} alt="icon" height="60" />
       </AppBar>
       <Grow in>
         <Container>
