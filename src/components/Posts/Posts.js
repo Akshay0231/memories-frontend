5373d5685983f7d367cf1b2a62be2cbf3efb52ee
Akshay0231 <akshaay2301@gmail.com>
2022-09-29 18:44:14 +0530

    Posts | styling property updated : MUI Grid

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -14,7 +14,7 @@ export const Posts = ({ setCurrentId }) => {
       <Grid className={classes.container} container alignItems="stretch" spacing={3}>
         {
           posts.map(post => (
-            <Grid key={post._id} item xs={12}>
+            <Grid key={post._id} item xs={12} sm={6} md={6}>
               <Post
                 setCurrentId={setCurrentId}
                 post={post}
